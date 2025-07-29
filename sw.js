b538931e018620211e8a0080bc5fb326d8c50802
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-07-29 16:35:35 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,16 +2,28 @@ const CACHE_NAME = "calculator-pwa-v1";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css",
   "./calculator.js",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
+  // Removed "./style.css" since you use inline styles
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting(); // Force activate
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  // Optional cleanup of old caches
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
   );
 });
 
